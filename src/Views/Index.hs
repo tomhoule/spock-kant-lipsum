1e8f78599fed4e5fd149affe8ec368ade535b264
Tom Houlé <tom@tomhoule.com>
2017-09-02 08:39:43 +0200

    Add acknowledgment

--- src/Views/Index.hs
+++ src/Views/Index.hs
@@ -8,4 +8,10 @@ import Data.Text.Lazy (toStrict)
 import Data.Text
 
 index :: Text
-index = toStrict $ renderHtml $ H.h1 $ H.text "O, hi"
+index = toStrict $ renderHtml $ H.html $ do
+    H.body $ do
+        H.h1 $ H.text "O, hi"
+        H.div $ do
+            H.text "This app was inspired by and uses code and samples from Enrico Gregorio’s"
+            H.text "kantlipsum package"
+            H.text "for LaTeX"
